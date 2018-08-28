bf89f457cb77e8180ce83bdf01c89f05720fb54b
javawaste <42322810+javawaste@users.noreply.github.com>
2018-08-28 17:58:07 +0530

    Add files via upload

---
+++ security-example/src/main/java/com/cg/config/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.cg.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+		     .withUser("pooja").password("pooja123").roles("USER");
+	}
+	
+	@Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+		httpSecurity
+		    .authorizeRequests()
+		    .anyRequest()
+		    .fullyAuthenticated()
+		    .and().httpBasic();
+		httpSecurity.csrf().disable();
+	}
+}
